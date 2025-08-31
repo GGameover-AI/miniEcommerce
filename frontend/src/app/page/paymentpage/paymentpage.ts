9875688d19d888c99f5db5be89bf56be87f64c98
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-08-31 13:18:20 +0700

    create paymentpage component

---
+++ frontend/src/app/page/paymentpage/paymentpage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentpage',
+  imports: [],
+  templateUrl: './paymentpage.html',
+  styleUrl: './paymentpage.css'
+})
+export class Paymentpage {
+
+}
